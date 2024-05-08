0bd4e976e951e5038e80c00815e8784faef21a9a
Kath <admin@kath.lol>
2024-05-08 12:33:11 +0800

    credits

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const fadeElements = document.querySelectorAll(".fade");
-const cursor = document.querySelector(".cursor");
+var colors = { default: 'rgba(237, 135, 150, 0.2)', hover: 'rgba(138, 173, 244, 0.6)' };
+const fadeElements = document.querySelectorAll('.fade');
+const cursor = document.querySelector('.cursor');
 
 fadeElements.forEach((fadeElement, index) => {
-  fadeElement.style.animationDelay = `${index * 0.75}s`;
+  fadeElement.style.animationDelay = `${index * 1}s`;
 });
 
-window.addEventListener("mousemove", function (e) {
+window.addEventListener('mousemove', function (e) {
   if (window.innerWidth >= 512) {
-    cursor.style.display = "block";
+    cursor.style.display = 'block';
     cursor.style.top = `${e.pageY - cursor.clientWidth / 2}px`;
     cursor.style.left = `${e.pageX - cursor.clientHeight / 2}px`;
   } else {
-    cursor.style.display = "none";
+    cursor.style.display = 'none';
   }
 });
 
-window.addEventListener("mouseover", function (e) {
+window.addEventListener('mouseover', function (e) {
   const classList = e.target.classList;
-  if (e.target.tagName === "A" || classList.contains("info")) {
-    cursor.style.background = "rgba(138, 173, 244, 0.6)";
-    cursor.style.transform = "scale(1)";
+  if (e.target.tagName === 'A' || classList.contains('info')) {
+    cursor.style.background = colors.hover;
+    cursor.style.transform = 'scale(1)';
   } else {
-    cursor.style.background = "rgba(237, 135, 150, 0.2)";
-    cursor.style.transform = "scale(0.8)";
+    cursor.style.background = colors.default;
+    cursor.style.transform = 'scale(0.8)';
   }
+});
+
+window.addEventListener('mousedown', function (e) {
+  cursor.style.background = colors.hover;
+  cursor.style.transform = 'scale(1)';
+});
+
+window.addEventListener('mouseup', function (e) {
+  cursor.style.background = colors.default;
+  cursor.style.transform = 'scale(0.8)';
 });
